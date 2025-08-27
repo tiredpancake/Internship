82f23619f76629d7c6b2fc690289668ea9fe1bbd
triedpancake <mahsa82.mo@gmail.com>
2025-08-27 20:26:52 +0330

    fix : try story book

--- chart_project/.storybook/main.js
+++ chart_project/.storybook/main.js
@@ -3,13 +3,10 @@
 /** @type { import('@storybook/react-webpack5').StorybookConfig } */
 const config = {
   "stories": [
-    "../src/**/*.mdx",
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
   "addons": [
-    "@storybook/preset-create-react-app",
-    "@storybook/addon-docs",
-    "@storybook/addon-onboarding"
+    "@storybook/preset-create-react-app"
   ],
   "framework": {
     "name": "@storybook/react-webpack5",
--- chart_project/src/App.js
+++ chart_project/src/App.js
@@ -1,7 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './Components/Header';
-import StatsTable from './Components/StatsTable';
+
 import StatsTableContainer from './pages/StatsTableContainer';
 function App() {
   return (
